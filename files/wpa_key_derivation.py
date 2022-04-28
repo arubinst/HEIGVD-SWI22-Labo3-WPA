05276d17bb10f134e9fd0b3627f188556bbac3fd
michael <michael.ruckstuhl@heig-vd.ch>
2022-04-28 15:49:15 +0200

    Pas propre du tout. Mais les variables sont trouvées

--- files/wpa_key_derivation.py
+++ files/wpa_key_derivation.py
@@ -23,6 +23,7 @@ from pbkdf2 import *
 from numpy import array_split
 from numpy import array
 import hmac, hashlib
+from scapy.contrib.wpa_eapol import WPA_key
 
 def customPRF512(key,A,B):
     """
@@ -38,26 +39,38 @@ def customPRF512(key,A,B):
     return R[:blen]
 
 # Read capture file -- it contains beacon, authentication, associacion, handshake and data
-wpa=rdpcap("wpa_handshake.cap") 
+wpa=rdpcap("wpa_handshake.cap")
+
+test1 = wpa[5][WPA_key]
+test2 = wpa[6][WPA_key]
+test3 = wpa[7][WPA_key]
+test4 = wpa[8][WPA_key]
 
 # Important parameters for key derivation - most of them can be obtained from the pcap file
 passPhrase  = "actuelle"
 A           = "Pairwise key expansion" #this string is used in the pseudo-random function
 ssid        = "SWI"
-APmac       = a2b_hex("cebcc8fdcab7")
-Clientmac   = a2b_hex("0013efd015bd")
+ssid = wpa[3].info.decode()
+#APmac       = a2b_hex("cebcc8fdcab7")
+APmac = a2b_hex(wpa[3].addr1.replace(':', ''))
+#Clientmac   = a2b_hex("0013efd015bd")
+Clientmac = a2b_hex(wpa[3].addr2.replace(':', ''))
 
 # Authenticator and Supplicant Nonces
-ANonce      = a2b_hex("90773b9a9661fee1f406e8989c912b45b029c652224e8b561417672ca7e0fd91")
-SNonce      = a2b_hex("7b3826876d14ff301aee7c1072b5e9091e21169841bce9ae8a3f24628f264577")
+#ANonce      = a2b_hex("90773b9a9661fee1f406e8989c912b45b029c652224e8b561417672ca7e0fd91")
+#SNonce      = a2b_hex("7b3826876d14ff301aee7c1072b5e9091e21169841bce9ae8a3f24628f264577")
+ANonce = test3.nonce
+SNonce = test4.nonce
 
 # This is the MIC contained in the 4th frame of the 4-way handshake
 # When attacking WPA, we would compare it to our own MIC calculated using passphrases from a dictionary
-mic_to_test = "36eef66540fa801ceee2fea9b7929b40"
+#mic_to_test = "36eef66540fa801ceee2fea9b7929b40"
+mic_to_test = test4.wpa_key_mic
 
 B           = min(APmac,Clientmac)+max(APmac,Clientmac)+min(ANonce,SNonce)+max(ANonce,SNonce) #used in pseudo-random function
 
-data        = a2b_hex("0103005f02030a0000000000000000000100000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000") #cf "Quelques détails importants" dans la donnée
+#data        = a2b_hex("0103005f02030a0000000000000000000100000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000") #cf "Quelques détails importants" dans la donnée
+data = bytes(test4.underlayer)
 
 print ("\n\nValues used to derivate keys")
 print ("============================")
@@ -67,6 +80,8 @@ print ("AP Mac: ",b2a_hex(APmac),"\n")
 print ("CLient Mac: ",b2a_hex(Clientmac),"\n")
 print ("AP Nonce: ",b2a_hex(ANonce),"\n")
 print ("Client Nonce: ",b2a_hex(SNonce),"\n")
+#print ("Data: ",b2a_hex(data),"\n")
+#print ("MIC to TEST: ",b2a_hex(mic_to_test),"\n")
 
 #calculate 4096 rounds to obtain the 256 bit (32 oct) PMK
 passPhrase = str.encode(passPhrase)
