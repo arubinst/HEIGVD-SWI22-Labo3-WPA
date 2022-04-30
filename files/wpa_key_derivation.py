a7401f28aa593682e836cd8dae0e63730f82ac95
JL-HEIG <jean-luc.blanc2@heig-vd.ch>
2022-04-30 22:51:45 +0200

    Task1 DONE

--- files/wpa_key_derivation.py
+++ files/wpa_key_derivation.py
@@ -15,6 +15,7 @@ __license__ 	= "GPL"
 __version__ 	= "1.0"
 __email__ 		= "abraham.rubinstein@heig-vd.ch"
 __status__ 		= "Prototype"
+__modified_by__ = "Blanc Jean-Luc & Plancherel Noémie"
 
 from scapy.all import *
 from binascii import a2b_hex, b2a_hex
@@ -43,21 +44,21 @@ wpa=rdpcap("wpa_handshake.cap")
 # Important parameters for key derivation - most of them can be obtained from the pcap file
 passPhrase  = "actuelle"
 A           = "Pairwise key expansion" #this string is used in the pseudo-random function
-ssid        = "SWI"
-APmac       = a2b_hex("cebcc8fdcab7")
-Clientmac   = a2b_hex("0013efd015bd")
+ssid        = wpa[0].info.decode('utf-8')
+APmac       = a2b_hex(wpa[5].addr2.replace(':', ''))
+Clientmac   = a2b_hex(wpa[5].addr1.replace(':', ''))
 
 # Authenticator and Supplicant Nonces
-ANonce      = a2b_hex("90773b9a9661fee1f406e8989c912b45b029c652224e8b561417672ca7e0fd91")
-SNonce      = a2b_hex("7b3826876d14ff301aee7c1072b5e9091e21169841bce9ae8a3f24628f264577")
+ANonce      = wpa[5].original[67:99]
+SNonce      = wpa[6].original[65:97]
 
 # This is the MIC contained in the 4th frame of the 4-way handshake
 # When attacking WPA, we would compare it to our own MIC calculated using passphrases from a dictionary
-mic_to_test = "36eef66540fa801ceee2fea9b7929b40"
+mic_to_test = wpa[8].original[-18:-2]
 
 B           = min(APmac,Clientmac)+max(APmac,Clientmac)+min(ANonce,SNonce)+max(ANonce,SNonce) #used in pseudo-random function
 
-data        = a2b_hex("0103005f02030a0000000000000000000100000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000") #cf "Quelques détails importants" dans la donnée
+data        = wpa[8].original[48:-18] + b'\0' * 16 + wpa[8].original[-2:]
 
 print ("\n\nValues used to derivate keys")
 print ("============================")
